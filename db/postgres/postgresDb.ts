8a236c20ac686f4e4e76fce037126ef1d49870f9
ukio <priz.a47@gmail.com>
2024-07-03 15:06:23 +0500

    fix

--- db/postgres/postgresDb.ts
+++ db/postgres/postgresDb.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 
-const sequelize:Sequelize = new Sequelize('postgres://secure:secure@localhost:5432/secure');
+const sequelize:Sequelize = new Sequelize('postgres://secure:secure@localhost:5432/secure',{logging:false});
 //const sequelize:Sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/ohrana',{logging:false});
     
 export default sequelize;
