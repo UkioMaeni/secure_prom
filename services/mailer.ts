ad313694fe0cc9c2621d3e4351e82bfe7a778f57
ukio <priz.a47@gmail.com>
2024-07-05 19:26:44 +0500

    fix

--- services/mailer.ts
+++ services/mailer.ts
@@ -91,7 +91,7 @@ export const imapFlowConnect=async()=>{
                     }
                     const find=await WhiteEmailList.findOne({
                         where:{
-                            [WhiteEmailListRow.email]:address.toLowerCase()
+                            [WhiteEmailListRow.email]:address
                         }
                     })
                     if(!find){
