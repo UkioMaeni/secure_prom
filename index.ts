c411093ad3b6b1c94386381dbd23b7aa1db7a9eb
ukio <priz.a47@gmail.com>
2024-10-15 17:00:21 +0500

    fix

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ process.on('uncaughtException', (err, origin) => {
   });
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors())
+app.use(cors()) 
 app.use("/api",dataRouter)
 app.use("/api",authRouter)
 app.use("/api",adminPanelRouter)
