009464a6f130fa01640778a4497e391e9cd85bc6
Ukio <priz.a47@gmail.com>
2025-09-04 11:41:50 +0500

    fix

--- services/initialize.ts
+++ services/initialize.ts
@@ -16,80 +16,85 @@ import JurnalHistory from '../models/jurnal_history';
 
 
 export async function initialize(){
-    await Jurnal.sync()
-    await JurnalHistory.sync()
-    await Settings.sync()
-    await Settings.findOrCreate({
-        where:{
-            [SettingsRow.name]:'lastMail'
-        },
-        defaults:{
-            [SettingsRow.name]:'lastMail',
-            [SettingsRow.value]:0
-        }
-    })
-    // await Settings.update({
-        
-    //         [SettingsRow.name]:'lastMail',
-    //         [SettingsRow.value]:0
-    //     },
-    //     {
-    //         where:{
-    //             [SettingsRow.name]:'lastMail',
-                
-    //         }
-    //     }
-    // )
-    await Settings.findOrCreate({
-        where:{
-            [SettingsRow.name]:'update'
-        },
-        defaults:{
-            [SettingsRow.name]:'update',
-            [SettingsRow.value]:0
-        }
-    })
-    await Settings.update(
-        {
-            [SettingsRow.value]:0
-        },
-        {
+    try {
+        await Jurnal.sync()
+        await JurnalHistory.sync()
+        await Settings.sync()
+        await Settings.findOrCreate({
+            where:{
+                [SettingsRow.name]:'lastMail'
+            },
+            defaults:{
+                [SettingsRow.name]:'lastMail',
+                [SettingsRow.value]:0
+            }
+        })
+        // await Settings.update({
+            
+        //         [SettingsRow.name]:'lastMail',
+        //         [SettingsRow.value]:0
+        //     },
+        //     {
+        //         where:{
+        //             [SettingsRow.name]:'lastMail',
+                    
+        //         }
+        //     }
+        // )
+        await Settings.findOrCreate({
             where:{
                 [SettingsRow.name]:'update'
+            },
+            defaults:{
+                [SettingsRow.name]:'update',
+                [SettingsRow.value]:0
             }
-        },
-       
-    );
-    await PbList.sync();
-    await WhiteEmailList.sync();
-    await WhiteEmailList.findOrCreate({
-        where:{
+        })
+        await Settings.update(
+            {
+                [SettingsRow.value]:0
+            },
+            {
+                where:{
+                    [SettingsRow.name]:'update'
+                }
+            },
+        
+        );
+        await PbList.sync();
+        await WhiteEmailList.sync();
+        await WhiteEmailList.findOrCreate({
+            where:{
+                
+            },defaults:{
+                [WhiteEmailListRow.email]:'priz.a47@gmail.com'
+            }
+        })
+        await Auth.sync({alter:true,});
+        await AdminAuth.sync()
+        await AdminAuth.findOrCreate({
+            where:{
+                [AdminAuthRow.login]:"root"
+            },
+            defaults:{
+                [AdminAuthRow.login]:"root",
+                [AdminAuthRow.pass_hash]:"4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2"
+            }
             
-        },defaults:{
-            [WhiteEmailListRow.email]:'priz.a47@gmail.com'
-        }
-    })
-    await Auth.sync({alter:true,});
-    await AdminAuth.sync()
-    await AdminAuth.findOrCreate({
-        where:{
-            [AdminAuthRow.login]:"root"
-        },
-        defaults:{
-            [AdminAuthRow.login]:"root",
-            [AdminAuthRow.pass_hash]:"4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2"
-        }
+        });
+        await Auth.findOrCreate({
+            where:{
+                [AuthRow.pass_hash]:"65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
+            },
+            defaults:{
+                [AuthRow.pass_hash]:"65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
+            }
+            
+        });
+        await FullInfo.sync({alter:true});
+    } catch (error) {
+        console.log(error);
         
-    });
-    await Auth.findOrCreate({
-        where:{
-            [AuthRow.pass_hash]:"65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
-        },
-        defaults:{
-            [AuthRow.pass_hash]:"65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
-        }
-        
-    });
-    await FullInfo.sync({alter:true});
+    }
     return;
 }
